1457a53bf7f5746823438eaa16ed26a341373db0
ALICIA <alicia.magro@gmail.com>
2025-02-09 11:54:18 +0100

    Navbar V1 is done

--- client/src/components/NavBar/NavBar.tsx
+++ client/src/components/NavBar/NavBar.tsx
@@ -3,7 +3,12 @@ import "./NavBar.css";
 function NavBar() {
   return (
     <nav className="navbar">
-      <h1> Portfolio</h1>
+      <ul className="nav-list">
+        <li className="nav-items">Présentation</li>
+        <li className="nav-items">Hard Skills</li>
+        <li className="nav-items">Soft Skills</li>
+        <li className="nav-items">Projets</li>
+      </ul>
     </nav>
   );
 }
