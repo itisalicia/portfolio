b9c3d7e762ea0edbcf6c4e35b69284e843ef1e6e
ALICIA <alicia.magro@gmail.com>
2025-02-10 16:23:55 +0100

    hard skills part is done

--- client/src/pages/HardSkills/HardSkills.tsx
+++ client/src/pages/HardSkills/HardSkills.tsx
@@ -1,71 +1,100 @@
+import { useState } from "react";
 import "./HardSkills.css";
 
 function HardSkills() {
+  const [frontEndSkills, setfrontEndSkills] = useState(false);
+  const [backEndSkills, setBackEndSkills] = useState(false);
+
+  const handleFrontClick = () => {
+    setfrontEndSkills(!frontEndSkills);
+  };
+
+  const handleBackClick = () => {
+    setBackEndSkills(!backEndSkills);
+  };
+
   return (
     <>
       <main id="hard-kills-main">
         <h2 className="hard-skills-title"> Hard Skills</h2>
         <p className="hard-skills-description">
           Aucune compétence n'est acquise sans travail. Voici les compétences
-          que j'ai acquises au fil de mes projets personnels et professionnels.
-          Des compétences techniques qui sont en constante évolution
+          que j'ai pu acquérir au fil de mes projets personnels et
+          professionnels. Des compétences techniques qui sont en constante
+          évolution et qui ne sont pas prêtes de s'arrêter...
         </p>
-        <h3 className="hard-skills-subtitle">Front-end</h3>
-        <section className="font-end-container">
-          <img
-            src="./img-hardkills/js.png"
-            alt="javascript logo"
-            className="hard-skills-logo"
-          />
-          <img
-            src="./img-hardkills/ts.png"
-            alt="javascript logo"
-            className="hard-skills-logo"
-          />
-          <img
-            src="./img-hardkills/react.png"
-            alt="javascript logo"
-            className="hard-skills-logo"
-          />
-          <img
-            src="./img-hardkills/html.png"
-            alt="html logo"
-            className="hard-skills-logo"
-          />
-          <img
-            src="./img-hardkills/css.png"
-            alt="css logo"
-            className="hard-skills-logo"
-          />
-          <img
-            src="./img-hardkills/figma.png"
-            alt="figma logo"
-            className="hard-skills-logo"
-          />
-        </section>
-        <h3 className="hard-skills-subtitle">Back-end</h3>
-        <section className="back-end-container">
-          <img
-            src="./img-hardkills/nodejs.png"
-            alt="node.js logo"
-            className="hard-skills-logo"
-          />
-          <img
-            src="./img-hardkills/express.png"
-            alt="express logo"
-            className="hard-skills-logo"
-          />
-          <img
-            src="./img-hardkills/sql.png"
-            alt="sql logo"
-            className="hard-skills-logo"
-          />
-          <img
-            src="./img-hardkills/mysql.png"
-            alt="mysql logo"
-            className="hard-skills-logo"
-          />
-        </section>
+        <h3
+          className="hard-skills-subtitle"
+          onClick={handleFrontClick}
+          onKeyUp={handleFrontClick}
+        >
+          Front-end
+        </h3>
+        {frontEndSkills && (
+          <section className="font-end-container">
+            <img
+              src="./img-hardkills/js.png"
+              alt="javascript logo"
+              className="hard-skills-logo"
+            />
+            <img
+              src="./img-hardkills/ts.png"
+              alt="javascript logo"
+              className="hard-skills-logo"
+            />
+            <img
+              src="./img-hardkills/react.png"
+              alt="javascript logo"
+              className="hard-skills-logo"
+            />
+            <img
+              src="./img-hardkills/html.png"
+              alt="html logo"
+              className="hard-skills-logo"
+            />
+            <img
+              src="./img-hardkills/css.png"
+              alt="css logo"
+              className="hard-skills-logo"
+            />
+            <img
+              src="./img-hardkills/figma.png"
+              alt="figma logo"
+              className="hard-skills-logo"
+            />
+          </section>
+        )}
+        <h3
+          className="hard-skills-subtitle"
+          onClick={handleBackClick}
+          onKeyUp={handleBackClick}
+        >
+          Back-end
+        </h3>
+        {backEndSkills && (
+          <section className="back-end-container">
+            <img
+              src="./img-hardkills/nodejs.png"
+              alt="node.js logo"
+              className="hard-skills-logo"
+            />
+            <img
+              src="./img-hardkills/express.png"
+              alt="express logo"
+              className="hard-skills-logo"
+            />
+            <img
+              src="./img-hardkills/sql.png"
+              alt="sql logo"
+              className="hard-skills-logo"
+            />
+            <img
+              src="./img-hardkills/mysql.png"
+              alt="mysql logo"
+              className="hard-skills-logo"
+            />
+          </section>
+        )}
       </main>
     </>
   );
