3a9493f2f7e5a1976da9463105fdb419ca0b5404
Alicia <alicia.magro@ouidou.fr>
2025-08-26 15:05:52 +0200

    feat: create introducing me page

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './components/header/header';
+import { IntroducingMe } from "./pages/introducing-me/introducing-me";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, IntroducingMe],
   templateUrl: './app.html',
   styleUrl: './app.css',
   host: {
