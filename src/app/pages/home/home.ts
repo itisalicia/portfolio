f7f645a5f996a5a7bd3c3877078cc33b22365135
Alicia <alicia.magro@gmail.com>
2025-09-03 11:39:26 +0200

    feat: start the experience section

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { IntroducingMe } from '../introducing-me/introducing-me';
 import { Skills } from "../skills/skills";
 import { Contact } from "../contact/contact";
+import { Experience } from '../experience/experience';
 
 @Component({
   selector: 'app-home',
-  imports: [IntroducingMe, Skills, Contact],
+  imports: [IntroducingMe, Skills, Contact, Experience],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
