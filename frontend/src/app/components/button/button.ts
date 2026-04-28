2d0665617d94810261eaf0fab8bc8bd697e5a927
Alicia Magro <alicia.magro@ouidou.fr>
2026-04-28 11:40:20 +0200

    feat: add burger menu for mobile version

--- frontend/src/app/components/button/button.ts
+++ frontend/src/app/components/button/button.ts
@@ -1,12 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-button',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './button.html',
   styleUrl: './button.css'
 })
 export class Button {
   @Input() text: string = "";
   @Input() type: 'button' | 'submit' | 'reset' = 'button';
+  @Input() small: boolean = false;
 }
